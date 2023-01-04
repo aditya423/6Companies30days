f6f05d2ab08c54d94be4ef8a54b4f76e350384bd
aditya423 <adityasghadge@gmail.com>
2023-01-04 23:21:05 +0530

    Day 04

--- Microsoft/09_Number_of_Pairs_Satisfying_Inequality.cpp
+++ Microsoft/09_Number_of_Pairs_Satisfying_Inequality.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
+        
+}
+
 int main() {
+
+    vector<int> nums1 = {3, 2, 5};
+    vector<int> nums2 = {2, 2, 1};
+    int diff = 1;
+
+    cout<<numberOfPairs(nums1, nums2, diff)<<endl;
 
     return 0;
 }
--- Microsoft/10_Shortest_Unsorted_Continuous_Subarray.cpp
+++ Microsoft/10_Shortest_Unsorted_Continuous_Subarray.cpp
@@ -1,7 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int findUnsortedSubarray(vector<int>& nums) {
+
+    // TLE --> Brute Force
+
+    // int l=nums.size(), r=0;
+    // for(int i=0; i<nums.size()-1; i++){
+    //     for(int j=i+1; j<nums.size(); j++){
+    //         if(nums[j] < nums[i]){
+    //             r = max(r, j);
+    //             l = min(l, i);
+    //         }
+    //     }
+    // }
+    // return (r-l<0) ? 0 : r-l+1;
+
+    // ACCEPTED --> Using Sorting
+
+    vector<int> snums(nums.begin(), nums.end());
+    sort(snums.begin(), snums.end());
+    int l=nums.size(), r=0;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] != snums[i]){
+            r = max(r, i);
+            l = min(l, i);
+        }
+    }
+    return (r-l<0) ? 0 : r-l+1; 
+}
+
 int main() {
+
+    vector<int> nums = {2,6,4,8,10,9,15};
+
+    cout<<findUnsortedSubarray(nums)<<endl;
 
     return 0;
 }
